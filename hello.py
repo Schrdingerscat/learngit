8f30be4a6e977e2865681dfbdad8a4bcf7546de0
Liang Yongjie <2524303785@qq.com>
2022-07-07 18:22:28 +0800

    A test for adding a python file

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("hello world\n")
+astring = "hello world"
+if (astring == "hello world"):
+    print("hello world !!!!!")
+elif (astring == "hello"):
+    print("sadness")
+else: 
+    print("hello world!!!!!not !!!!!")
+
+cnt = 0
+while (cnt <10):
+    print(astring)
+    print("\n")
+    cnt += 1
+
+print ("This is the end of the programme")
+
+print("===================================")
+
